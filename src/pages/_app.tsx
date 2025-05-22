f74cd2ada8461c12efcde17aa9329efeb5fa43a0
Daniel Rossmann <danielrossmann@a22ebbff-5178-4916-9d82-64d46e01df5d.fritz.box>
2025-05-22 18:00:37 +0200

    Fix layout for Pages Router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-import { Toaster } from "sonner"
+// src/pages/_app.tsx
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Toaster richColors position="top-center" />
+    <AuthProvider>
       <Component {...pageProps} />
-    </>
-  )
+    </AuthProvider>
+  );
 }
